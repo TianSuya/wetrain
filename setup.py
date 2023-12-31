64c3bcfc1fb5528b412b4e577b460df2cc1cf9a0
tbw <t18637403315@163.com>
2023-12-31 12:58:42 +0800

    1.0.0 commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='wetrain',
+    version='0.0.1',
+    author='tbw',
+    author_email='t18637403315@163.com',
+    description='a simple deep learning framework',
+    packages=setuptools.find_packages()
+)
